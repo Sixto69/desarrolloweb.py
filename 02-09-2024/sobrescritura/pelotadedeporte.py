cd190a8b288c09da2e1d54b44ee6b5617d864d7e
Sixto Obreque <sixto.obreque@gmail.com>
2024-09-07 19:44:25 -0400

    se modifica codigo

---
+++ 02-09-2024/sobrescritura/pelotadedeporte.py
@@ -0,0 +1,38 @@
+#Sobreescritura haciendo llamado a método de clase padre
+class PelotaDeDeporte():
+    def __init__(self, color: str):
+        self.__color = color
+
+    @property
+    def color(self):
+        return self.__color
+    
+    @color.setter
+    def color(self, color):
+        self.__color = color
+
+class PelotaDeFutbol(PelotaDeDeporte):
+    def __init__(self, color: str, cantidad_hexagonos: int):
+        super().__init__(color) # se ejecuta constructor de PelotaDeDeporte
+        self.__cantidad_hexagonos = cantidad_hexagonos
+
+    @property
+    def cantidad_hexagonos(self):
+        return self.__cantidad_hexagonos
+    
+    @property
+    def color(self):
+        return f"{super().color} al estilo Pelota de Futbol"
+    
+"""
+la pelota de futbol sabe obtener el color como su padre, pero al sobreescribirlo lo hace a su manera
+""" 
+    
+
+
+
+pdf = PelotaDeFutbol("Blanco y Negro", 15)
+# Salida: Blanco y Negro
+print(pdf.color)
+# Salida: 15
+print(pdf.cantidad_hexagonos)
---
+++ 02-09-2024/sobrescritura/pelotadedeporte4.py
@@ -0,0 +1,39 @@
+##Parámetro especial **kwargs
+class PelotaDeDeporte():
+    def __init__(self, tamaño: int, **kwargs):
+        super().__init__(**kwargs)
+        print("Creando pelota de deporte")
+        self.tamaño = tamaño
+
+class PelotaDePlastico():
+    def __init__(self, material: str, **kwargs):
+        super().__init__(**kwargs)
+        print("Creando pelota de plástico")
+        self.material = material
+
+class PelotaDePingPong(PelotaDeDeporte, PelotaDePlastico):
+    def __init__(self, timbre: str, **kwargs):
+        super().__init__(**kwargs)
+        print("Creando pelota de ping pong")
+        self.timbre = timbre
+
+    def __str__(self):
+        return f"{self.tamaño} {self.material} {self.timbre}"
+
+class PelotaDeTennis(PelotaDeDeporte, PelotaDePlastico):
+    def __init__(self, textura: str, **kwargs):
+        super().__init__(**kwargs)
+        print("Creando pelota de tennis")
+        self.textura = textura
+
+    def __str__(self):
+        return f"{self.tamaño} {self.material} {self.textura}"   
+    
+# Salida:
+# Creando pelota de plástico
+# Creando pelota de deporte
+# Creando pelota de ping pong
+pdpp = PelotaDePingPong(tamaño=4, material="celuloide",timbre="POWERTI")
+print(pdpp)
+pdt = PelotaDeTennis(tamaño=10, material="caucho", textura="rugosa")
+print(pdt)
